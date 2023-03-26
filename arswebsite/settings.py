4a12d93a488932015d93a6da812dcbd328390e3f
dovicin <125053912+dovicin@users.noreply.github.com>
2023-03-26 15:32:13 +0300

    Update settings.py

--- arswebsite/settings.py
+++ arswebsite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = BASE_DIR / "uploads"
-MEDIA_URL = "/img"
+MEDIA_URL = "/img/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
